34ca3a7387e3d33eb0ad2ff63a292b7e02f54d03
udit <itgetjobs4@gmail.com>
2020-01-06 12:28:39 +0530

    added buttons

--- app/src/main/java/com/example/integrategithub/MainActivity.java
+++ app/src/main/java/com/example/integrategithub/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.integrategithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView text1;
+    Button button1,button2,button3,button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text1=findViewById(R.id.text1);
+        button1=findViewById(R.id.button1);
+        button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+        button4=findViewById(R.id.button4);
+
+
     }
 }

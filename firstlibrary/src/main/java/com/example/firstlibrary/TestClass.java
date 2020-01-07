1d60c90e48a0b1c754faaad699297b45cb2b99ca
udit <itgetjobs4@gmail.com>
2020-01-07 15:44:59 +0530

    lib add

---
+++ firstlibrary/src/main/java/com/example/firstlibrary/TestClass.java
@@ -0,0 +1,11 @@
+package com.example.firstlibrary;
+
+import android.util.Log;
+
+public class TestClass {
+
+    public static void main(String[] args){
+
+        Log.i("PrintCheck","Printing success");
+    }
+}
